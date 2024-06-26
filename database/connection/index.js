f935d3445c2c860e5949d89b418cf0838533a263
Arun Patel <ap75766@gmail.com>
2024-06-26 17:07:46 +0530

    updates

--- database/connection/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { DB_URL } from '../../config/index.js';
-export const connectDB = () => {
-    mongoose.connect(DB_URL).then(() => {
-        console.log("Db Connected");
-    }).catch(() => {
-        console.log("Db is not connected");
-    });
-};
